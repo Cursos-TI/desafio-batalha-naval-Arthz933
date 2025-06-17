3be7c01353d61b2a0d1625323b4cb39b9fbb503c
Arthz933 <arthurassuncao017@gmail.com>
2025-06-17 13:51:30 +0000

    concluir sobreposição das matrizes

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,7 +6,7 @@ int funcaobatalhanaval() {
     int matriznavios[10][10] = {0};  // Inicializa todos com 0
     
 
-    matriznavios[4][2] = 3; //
+    /* matriznavios[4][2] = 3; //
     matriznavios[3][2] = 3; // coordenadas navio vertical
     matriznavios[2][2] = 3; // 
     matriznavios[7][6] = 3; //
@@ -17,7 +17,10 @@ int funcaobatalhanaval() {
     matriznavios[5][6] = 3; // 
     matriznavios[6][1] = 3; //
     matriznavios[7][2] = 3; // coordenadas navio diagonal esquerda
-    matriznavios[8][3] = 3; // 
+    matriznavios[8][3] = 3; // */
+
+
+
 
 
     for(int c = 0; c <= 10; c++) { // mostra as linha de identificação com números 
@@ -38,42 +41,25 @@ int funcaobatalhanaval() {
 
 
     printf("dica: sempre considere as coordenadas como um valor a menos, matrizes sempre começam do 0\n");
+
     
-    printf("----------------\n");
-    printf("matriz cone\n");
-    
-    int matrizhabilidadecone[4][7] = {0};
+    int matrizhabilidadecone[3][5] = 
+    {0, 0, 1, 0, 0,
+     0, 1, 1, 1, 0,
+     1, 1, 1, 1, 1, };
 
-    for(int c = 0; c < 7; c++) { // muda os valores da matriz para 1, conforme as áreas da habilidade
-        matrizhabilidadecone[3][c] = 1;
-        for(int c = 1; c < 6; c++)
-            matrizhabilidadecone[2][c] = 1;
-            for(int c = 2; c < 5; c++) {
-                matrizhabilidadecone[1][c] = 1;
-                for(int c = 3; c < 4; c++) {
-                    matrizhabilidadecone[0][c] = 1;
 
-                }
-
-            }
-            
-
-    }
     
 
-    for(int l = 0; l < 4; l++) {
+    /* for(int l = 0; l < 4; l++) {
         for(int c = 0; c < 7; c++) {
             printf("%d ", matrizhabilidadecone[l][c]); // imprime a matriz
 
         }
         printf("\n");
 
-    }    
+    } */
 
-    
-
-    printf("----------------\n");
-    printf("matriz cruz\n");
 
 
     int matrizhabilidadecruz[5][5] = {0};
@@ -85,18 +71,15 @@ int funcaobatalhanaval() {
         }
     }
 
-    for(int l = 0; l < 5; l++) {
+    /*for(int l = 0; l < 5; l++) {
         for(int c = 0; c < 5; c++) {
             printf("%d ", matrizhabilidadecruz[l][c]);
 
         }
         printf("\n");
-    }
+    } */
 
     
-
-    printf("----------------\n");
-    printf("matriz octaedro\n");
 
     int matrizhabilidadeoctaedro[7][9] = {0};
 
@@ -122,9 +105,50 @@ int funcaobatalhanaval() {
         }
     }
 
-    for(int l = 0; l < 7; l++) {
+    /* for(int l = 0; l < 7; l++) {
         for(int c = 0; c < 9; c++) {
             printf("%d ", matrizhabilidadeoctaedro[l][c]);
+        }
+        printf("\n");
+    } */
+
+
+    int linhainicio = 0; 
+    int colunainicio = 5;
+    for(int l = 0; l < 3; l++) {
+        for(int c = 0; c < 5; c++) {
+            if(linhainicio + 3 < 10 && colunainicio + 5 < 10) {
+                if(matrizhabilidadecone[l][c] != 0) {
+                matriznavios[linhainicio + l][colunainicio + c] = matrizhabilidadecone[l][c];
+            } 
+            }
+            
+        }
+    }
+
+    int linhainicio = 3;
+    int colunainicio = 8;
+    for(int l = 0; l < 5; l++) {
+        for(int c = 0; c < 5; c++) {
+            if(linhainicio + 5 < 10 && colunainicio < 10)
+            if(matrizhabilidadecone[l][c] != 0) {
+                matriznavios[l + linhainicio][c + colunainicio] = matrizhabilidadecruz[l][c];
+            } 
+
+        }
+    }
+    for(int l = 0; l < 5; l++) {
+        for(int c = 0; c < 5; c++) {
+            if(matrizhabilidadecone[l][c] != 0) {
+                matriznavios[l + linhainicio][c + colunainicio] = matrizhabilidadeoctaedro[l][c];
+            } 
+
+        }
+    }
+
+    for (int l = 0; l < 10; l++) {
+        for (int c = 0; c < 10; c++) {
+            printf("%d ", matriznavios[l][c]);
         }
         printf("\n");
     }
