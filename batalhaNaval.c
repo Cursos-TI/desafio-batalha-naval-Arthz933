81a4633efb54234ce94fb1b4469eef3384551b95
Arthz933 <arthurassuncao017@gmail.com>
2025-06-18 13:59:09 +0000

    concluir sobreposição das matrizes cone e octaedro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,6 +5,48 @@ int funcaobatalhanaval() {
 
     int matriznavios[10][10] = {0};  // Inicializa todos com 0
     
+    int matrizcone[3][5] = {0};
+        for(int c = 0; c < 5; c++) {
+            matrizcone[5][c] = 5;
+            
+            for(int l = 0; l < 3; l++) {
+            
+            }
+            
+        }
+        matrizcone[4][7] = 5;
+        matrizcone[4][8] = 5;
+        matrizcone[4][9] = 5;
+        matrizcone[3][8] = 5;
+
+        int linhainiciocone = 0; 
+        int colunainiciocone = 0;
+
+        for(int l = 0; l < 3; l++) {
+            for(int c = 0; c < 5; c++) {
+            matriznavios[linhainiciocone + l][colunainiciocone + c] = matrizcone[l][c];
+
+            }
+        } 
+    int matrizcruz[5][5] = {0};
+        for(int c = 0; c < 5; c++) { // monta a cruz
+            matrizcruz[1][c] = 5;
+        }
+        for(int l = 0; l < 5; l++) {
+            matrizcruz[l][2] = 5;
+
+            }
+
+        int linhainiciocruz = 0; 
+        int colunainiciocruz = 0; 
+        for (int l = 0; l < 5; l++) {
+            for (int c = 0; c < 5; c++) {
+            matriznavios[linhainiciocruz + l][colunainiciocruz + c] = matrizcruz[l][c]; // sobrepõe a matriz cruz na matriz
+        }
+    } /*como a linhainicio e a colunainicio são 0, ele considera as coordenadas da estrutura que monta a cruz, 
+    no caso, l [1] e c [2]*/ 
+    int matrizoctaedro[5][5] = {0};
+
 
     /* matriznavios[4][2] = 3; //
     matriznavios[3][2] = 3; // coordenadas navio vertical
@@ -19,18 +61,18 @@ int funcaobatalhanaval() {
     matriznavios[7][2] = 3; // coordenadas navio diagonal esquerda
     matriznavios[8][3] = 3; // */
 
+    
+    
 
 
-
-
-    for(int c = 0; c <= 10; c++) { // mostra as linha de identificação com números 
+    for(int c = 0; c <= 10; c++) { // mostra as coluna de identificação com números 
         printf("%d ", c);
     }
 
     printf("\n");
 
 
-    for (int l = 0; l < 10; l++) { // mostra as coluna de identificação com números
+    for (int l = 0; l < 10; l++) { // mostra as linha de identificação com números
         printf("%d ", l);
 
         for (int c = 0; c < 10; c++) {
@@ -43,41 +85,29 @@ int funcaobatalhanaval() {
     printf("dica: sempre considere as coordenadas como um valor a menos, matrizes sempre começam do 0\n");
 
     
-    int matrizhabilidadecone[3][5] = 
-    {0, 0, 1, 0, 0,
-     0, 1, 1, 1, 0,
-     1, 1, 1, 1, 1, };
+    /*int matrizhabilidadecone[3][5] = {0};
 
 
-    
-
-    /* for(int l = 0; l < 4; l++) {
+     for(int l = 0; l < 4; l++) {
         for(int c = 0; c < 7; c++) {
             printf("%d ", matrizhabilidadecone[l][c]); // imprime a matriz
 
         }
         printf("\n");
 
-    } */
+    } 
 
 
 
-    int matrizhabilidadecruz[5][5] = {0};
+    
 
     for(int l = 0; l < 5; l++) {
-        matrizhabilidadecruz[l][2] = 1;
-        for(int c = 0; c < 5; c++) {
-            matrizhabilidadecruz[2][c] = 1;
-        }
-    }
-
-    /*for(int l = 0; l < 5; l++) {
         for(int c = 0; c < 5; c++) {
             printf("%d ", matrizhabilidadecruz[l][c]);
 
         }
         printf("\n");
-    } */
+    } 
 
     
 
@@ -110,49 +140,9 @@ int funcaobatalhanaval() {
             printf("%d ", matrizhabilidadeoctaedro[l][c]);
         }
         printf("\n");
-    } */
+    } 
 
-
-    int linhainicio = 0; 
-    int colunainicio = 5;
-    for(int l = 0; l < 3; l++) {
-        for(int c = 0; c < 5; c++) {
-            if(linhainicio + 3 < 10 && colunainicio + 5 < 10) {
-                if(matrizhabilidadecone[l][c] != 0) {
-                matriznavios[linhainicio + l][colunainicio + c] = matrizhabilidadecone[l][c];
-            } 
-            }
-            
-        }
-    }
-
-    int linhainicio = 3;
-    int colunainicio = 8;
-    for(int l = 0; l < 5; l++) {
-        for(int c = 0; c < 5; c++) {
-            if(linhainicio + 5 < 10 && colunainicio < 10)
-            if(matrizhabilidadecone[l][c] != 0) {
-                matriznavios[l + linhainicio][c + colunainicio] = matrizhabilidadecruz[l][c];
-            } 
-
-        }
-    }
-    for(int l = 0; l < 5; l++) {
-        for(int c = 0; c < 5; c++) {
-            if(matrizhabilidadecone[l][c] != 0) {
-                matriznavios[l + linhainicio][c + colunainicio] = matrizhabilidadeoctaedro[l][c];
-            } 
-
-        }
-    }
-
-    for (int l = 0; l < 10; l++) {
-        for (int c = 0; c < 10; c++) {
-            printf("%d ", matriznavios[l][c]);
-        }
-        printf("\n");
-    }
-
+*/
 };
 
 
