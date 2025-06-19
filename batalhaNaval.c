484e01b1a1591942d35a3a1e67d485da31c2b8c8
Arthz933 <arthurassuncao017@gmail.com>
2025-06-19 20:19:15 +0000

    finalizar a sobreposição das matrizes octaedro e cone

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,47 +5,26 @@ int funcaobatalhanaval() {
 
     int matriznavios[10][10] = {0};  // Inicializa todos com 0
     
-    int matrizcone[3][5] = {0};
-        for(int c = 0; c < 5; c++) {
-            matrizcone[5][c] = 5;
-            
-            for(int l = 0; l < 3; l++) {
-            
-            }
-            
-        }
-        matrizcone[4][7] = 5;
-        matrizcone[4][8] = 5;
-        matrizcone[4][9] = 5;
-        matrizcone[3][8] = 5;
+    int matrizhabilidades[10][10] = {0};
+    /*int matriz cone[3][5];
+    int matriz cruz[5][5];
+    int matriz octaedro[5][5];*/
 
-        int linhainiciocone = 0; 
-        int colunainiciocone = 0;
-
-        for(int l = 0; l < 3; l++) {
-            for(int c = 0; c < 5; c++) {
-            matriznavios[linhainiciocone + l][colunainiciocone + c] = matrizcone[l][c];
-
-            }
-        } 
-    int matrizcruz[5][5] = {0};
         for(int c = 0; c < 5; c++) { // monta a cruz
-            matrizcruz[1][c] = 5;
+            matrizhabilidades[1][c] = 5;
         }
         for(int l = 0; l < 5; l++) {
-            matrizcruz[l][2] = 5;
+            matrizhabilidades[l][2] = 5;
 
             }
 
-        int linhainiciocruz = 0; 
-        int colunainiciocruz = 0; 
         for (int l = 0; l < 5; l++) {
             for (int c = 0; c < 5; c++) {
-            matriznavios[linhainiciocruz + l][colunainiciocruz + c] = matrizcruz[l][c]; // sobrepõe a matriz cruz na matriz
+                matriznavios[l][c] = matrizhabilidades[l][c];
         }
-    } /*como a linhainicio e a colunainicio são 0, ele considera as coordenadas da estrutura que monta a cruz, 
-    no caso, l [1] e c [2]*/ 
-    int matrizoctaedro[5][5] = {0};
+    }
+
+
 
 
     /* matriznavios[4][2] = 3; //
@@ -82,67 +61,8 @@ int funcaobatalhanaval() {
     }
 
 
-    printf("dica: sempre considere as coordenadas como um valor a menos, matrizes sempre começam do 0\n");
+    printf("dica: matrizes sempre começam do 0\n");
 
-    
-    /*int matrizhabilidadecone[3][5] = {0};
-
-
-     for(int l = 0; l < 4; l++) {
-        for(int c = 0; c < 7; c++) {
-            printf("%d ", matrizhabilidadecone[l][c]); // imprime a matriz
-
-        }
-        printf("\n");
-
-    } 
-
-
-
-    
-
-    for(int l = 0; l < 5; l++) {
-        for(int c = 0; c < 5; c++) {
-            printf("%d ", matrizhabilidadecruz[l][c]);
-
-        }
-        printf("\n");
-    } 
-
-    
-
-    int matrizhabilidadeoctaedro[7][9] = {0};
-
-    for(int c = 4; c < 5; c++) {
-        matrizhabilidadeoctaedro[0][4] = 1;
-        for(int c = 3; c < 6; c++) {
-            matrizhabilidadeoctaedro[1][c] = 1;
-            for(int c = 2; c < 7; c++) {
-                matrizhabilidadeoctaedro[2][c] = 1;
-                for(int c = 1; c < 8; c++) {
-                    matrizhabilidadeoctaedro[3][c] = 1;
-                for(int c = 2; c < 7; c++) {
-                    matrizhabilidadeoctaedro[4][c] = 1;
-            for(int c = 3; c < 6; c++) {
-                matrizhabilidadeoctaedro[5][c] = 1;
-        for(int c = 4; c < 5; c++) {
-            matrizhabilidadeoctaedro[6][c] = 1;
-        }
-            }
-                }
-                }
-            }
-        }
-    }
-
-    /* for(int l = 0; l < 7; l++) {
-        for(int c = 0; c < 9; c++) {
-            printf("%d ", matrizhabilidadeoctaedro[l][c]);
-        }
-        printf("\n");
-    } 
-
-*/
 };
 
 
